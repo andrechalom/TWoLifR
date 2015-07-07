d5339f0ac071971996ca26cf5bb1fb986add3f9c
Andre Chalom <andrechalom@gmail.com>
2015-07-07 20:43:59 -0300

    Separated land generating functions

--- R/Landscape.R
+++ R/Landscape.R
@@ -6,7 +6,7 @@
 #' @param type character. Currently, three types of habitat fragmentation routines are implemented: random fragmentation (noise), contiguous blob removal (random walk removal) and Fahrig fragmentation.
 #' @param land.shape,bound.condition character. Control whether the shape of the land should be treated as a square (default) or a circle inscribed in this square; and what is the boundary condition. These parameters have no effect on the actual landscape generated, but will be used passed to the simulation.
 #' @param cover numerical, between 0 and 1. What fraction of the landscape should consist on habitat?
-#' @param frag recuperation parameter for the Fahrig method. Is ignored for other types. Higher values 
+#' @param frag fragmentation parameter for the Fahrig method. Is ignored for other types. Higher values 
 #' are close to random noise, while lower values result in more realistic landshapes.
 #' @examples
 #' plot(Landscape(cover=0.7, type="f", frag=0.01))
@@ -23,80 +23,12 @@ Landscape <- function (numb.cells = 100, land.shape = c("square","circle"),
   if (type=="fahrig" && is.null(frag))
     stop("frag parameter must be specified for fahrig fragmentation!")
 	# scape represents the actual landscape
-	scape <- rep(1, numb.cells*numb.cells)
-	if(cover < 1) {
-		NtoRemove=round((1-cover)*numb.cells*numb.cells);
-		if(type=="random") {
-			while(NtoRemove>0)
-			{
-				i=round(runif(1,0,numb.cells-1));
-				j=round(runif(1,0,numb.cells-1));
-				# tests to see if this point has already been removed
-				if(scape[1+numb.cells*j+i] == 1) {
-					NtoRemove = NtoRemove - 1
-					scape[1+numb.cells*j+i] = 0
-				}
-			}
-		}
-		if(type=="blob") {
-			i=round(runif(1,0,numb.cells-1));
-			j=round(runif(1,0,numb.cells-1));
-			while(NtoRemove>0)
-			{
-				# tests to see if this point has already been removed
-				if(scape[1+numb.cells*j+i] == 1) {
-					NtoRemove = NtoRemove - 1
-					scape[1+numb.cells*j+i] = 0
-				}
-				# Draft a new point to be removed (random walk!)
-				if(sample(1:2,1) == 1) {
-					i = i + (-1)**sample(1:2,1)
-				} else {
-					j = j + (-1)**sample(1:2,1)
-				}
-				if(i == -1) { i=numb.cells-1}
-				if(i == numb.cells) { i=1}
-				if(j == -1) { j=numb.cells-1}
-				if(j == numb.cells) { j=1}
-			}
-		}
-    if(type=="fahrig")
-    {
-      scape=matrix(rep(0, numb.cells*numb.cells),numb.cells,numb.cells)
-      while(sum(scape)<round(numb.cells*numb.cells*cover))
-      {
-        chosen.cell=sample(1:numb.cells,2,replace=T)
-        neigh.cells=matrix(c(chosen.cell[1],chosen.cell[2]+1,chosen.cell[1]-1,chosen.cell[2],chosen.cell[1],
-                             chosen.cell[2]-1,chosen.cell[1]+1,chosen.cell[2]),4,2,byrow=T)
-        out.land=which(neigh.cells==numb.cells+1 | neigh.cells==0,arr.ind=T)[,1]
-        n.neigh=1:4
-        if(scape[chosen.cell[1],chosen.cell[2]]==0) 
-        {
-          rnd=runif(1,0,1)
-          is.habitat=0
-          if(length(out.land)>0)
-          {
-            for(i in n.neigh[-out.land])
-            {
-              is.habitat=is.habitat+scape[neigh.cells[i,1],neigh.cells[i,2]]
-            }
-          }
-          else
-          {
-            for(i in n.neigh)
-            {
-              is.habitat=is.habitat+scape[neigh.cells[i,1],neigh.cells[i,2]]
-            }
-          }
-          if(rnd<frag | is.habitat>0) 
-          {
-            scape[chosen.cell[1],chosen.cell[2]]=1
-          }
-        }  
-      }
-      scape=as.numeric(scape)
-    }
-	}
+	if(cover == 1) scape <- rep(1, numb.cells*numb.cells)
+  else {
+		if(type=="random") scape <- randomLandscape(numb.cells, cover)
+		if(type=="blob") scape <- blobLandscape(numb.cells, cover)
+    if(type=="fahrig") scape <- fahrigLandscape(numb.cells, cover, frag)
+  }
   land <- new.env()
   land$numb.cells <- numb.cells;  land$land.shape <- land.shape;
   land$cover <- cover;
@@ -126,4 +58,86 @@ plot.landscape <- function(x, col1="darkgreen", col2="grey70", ...) {
  		color = c(col2, col1)
  	}
  	image(s, s, matrix(x$scape,ncol=n), col=color, ...)
+}
+
+
+### Landscape generating functions
+### Internal methods
+
+randomLandscape <- function(numb.cells, cover) {
+	scape <- rep(1, numb.cells*numb.cells)
+  NtoRemove=round((1-cover)*numb.cells*numb.cells);
+  while(NtoRemove>0)
+  {
+    i=round(runif(1,0,numb.cells-1));
+    j=round(runif(1,0,numb.cells-1));
+    # tests to see if this point has already been removed
+    if(scape[1+numb.cells*j+i] == 1) {
+      NtoRemove = NtoRemove - 1
+      scape[1+numb.cells*j+i] = 0
+    }
+  }
+  return (scape)
+}
+
+blobLandscape <- function(numb.cells, cover) {
+	scape <- rep(1, numb.cells*numb.cells)
+  NtoRemove=round((1-cover)*numb.cells*numb.cells);
+  i=round(runif(1,0,numb.cells-1));
+  j=round(runif(1,0,numb.cells-1));
+  while(NtoRemove>0)
+  {
+    # tests to see if this point has already been removed
+    if(scape[1+numb.cells*j+i] == 1) {
+      NtoRemove = NtoRemove - 1
+      scape[1+numb.cells*j+i] = 0
+    }
+    # Draft a new point to be removed (random walk!)
+    if(sample(1:2,1) == 1) {
+      i = i + (-1)**sample(1:2,1)
+    } else {
+      j = j + (-1)**sample(1:2,1)
+    }
+    if(i == -1) { i=numb.cells-1}
+    if(i == numb.cells) { i=1}
+    if(j == -1) { j=numb.cells-1}
+    if(j == numb.cells) { j=1}
+  }
+  return(scape)
+}
+
+fahrigLandscape <- function(numb.cells, cover, frag) {
+  scape=matrix(rep(0, numb.cells*numb.cells),numb.cells,numb.cells)
+  while(sum(scape)<round(numb.cells*numb.cells*cover))
+  {
+    chosen.cell=sample(1:numb.cells,2,replace=T)
+    neigh.cells=matrix(c(chosen.cell[1],chosen.cell[2]+1,chosen.cell[1]-1,chosen.cell[2],chosen.cell[1],
+                         chosen.cell[2]-1,chosen.cell[1]+1,chosen.cell[2]),4,2,byrow=T)
+    out.land=which(neigh.cells==numb.cells+1 | neigh.cells==0,arr.ind=T)[,1]
+    n.neigh=1:4
+    if(scape[chosen.cell[1],chosen.cell[2]]==0) 
+    {
+      rnd=runif(1,0,1)
+      is.habitat=0
+      if(length(out.land)>0)
+      {
+        for(i in n.neigh[-out.land])
+        {
+          is.habitat=is.habitat+scape[neigh.cells[i,1],neigh.cells[i,2]]
+        }
+      }
+      else
+      {
+        for(i in n.neigh)
+        {
+          is.habitat=is.habitat+scape[neigh.cells[i,1],neigh.cells[i,2]]
+        }
+      }
+      if(rnd<frag | is.habitat>0) 
+      {
+        scape[chosen.cell[1],chosen.cell[2]]=1
+      }
+    }  
+  }
+  return(as.numeric(scape))
 }
